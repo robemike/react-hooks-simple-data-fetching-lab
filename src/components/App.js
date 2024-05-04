4935ec95e6f1f389850328bac3a215aacd61a7e1
robemike <robemike1135@gmail.com>
2024-05-04 12:08:08 +0300

    completes tests

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+// import { useEffect, useState } from "react"
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => setImage(data.message))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  if (!image) {
+    return <p>Loading...</p>;
+  }
+
+  return <img alt="A Random Dog" src={image} />;
+}
+
+export default App;
